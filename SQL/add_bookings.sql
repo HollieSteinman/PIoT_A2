51e287cd41bb828c27b458326fb76ca677dc0049
lukedavoli <s3782747@student.rmit.edu.au>
2020-05-03 15:00:14 +1000

    Added SQL for creating tables and adding dummy data

---
+++ SQL/add_bookings.sql
@@ -0,0 +1,4 @@
+INSERT INTO booking (car_id, customer_id, start_datetime, end_datetime, status)
+VALUES 	(3, 2, '2020-05-24 09:00:00', '2020-05-24 17:30:00', 'active'),
+		(4, 1, '2020-05-01 19:00:00', '2020-05-02 13:30:00', 'complete'),
+        (5, 4, '2020-04-03 17:00:00', '2020-04-10 17:00:00', 'cancelled');
---
+++ SQL/add_cars.sql
@@ -0,0 +1,6 @@
+INSERT INTO car (status, make, model, body_type, colour, seats, location, cost_per_hour)
+VALUES 	('available', 'Volkswagen', 'Passat', 'sedan', 'grey', 5,  '-37.766720,145.137650', 33),
+		('available', 'Subaru', 'WRX', 'sedan', 'blue', 5,  '-37.781078,145.100199', 3),
+        ('unavailable', 'Ford', 'Fiesta', 'hatchback', 'silver', 5,  '-37.766720,145.137650', 15),
+        ('unavailable', 'Volkswagen', 'Tiguan', 'SUV', 'white', 7,  '-37.709227,145.109037', 45),
+        ('available', 'Ferrari', '488', 'coupe', 'red', 2, '34.625479,-97.212649', 148);
