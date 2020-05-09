22503f0ee239c524a653a992113533b39753598e
Matthies Abera <s3779748@student.rmit.edu.au>
2020-05-09 21:18:46 +1000

    created search devices + check pi found methods

--- notification.py
+++ notification.py
@@ -1,10 +1,29 @@
 import os, requests, json, string, bluetooth
 
-print ("Searching for devices...")
-
 nearby_devices = bluetooth.discover_devices(lookup_names = True)
 
-print ("found %d devices" % len(nearby_devices))
+def discoverDevices():
+     print("found %d device(s)" % len(nearby_devices))
 
-for name, addr in nearby_devices:
-     print (" %s - %s" % (addr, name))
+     for name, addr in nearby_devices:
+          print (" %s - %s" % (addr, name))
+
+
+# named raspberry pi for testing 
+# replace name with master pi
+
+def masterPiFound():
+     found = False
+
+     for addr, name in nearby_devices:
+          if(name == 'Matthies\’s iPhone' or 'raspberrypi'):
+               found = True
+
+     return found
+
+def sendNotification():
+     if(masterPiFound()):
+          print("Car Unlocked")
+
+
+sendNotification()
