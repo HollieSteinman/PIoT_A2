4d9c7669b6dbe60fdf78a11171de6cc6f713a1dc
[Michael Sartorel s3786267] <[s3786267@student.rmit.edu.au]>
2020-05-10 16:58:31 +1000

    Added more cars

--- SQL/add_cars.sql
+++ SQL/add_cars.sql
@@ -1,6 +1,9 @@
 INSERT INTO car (status, make, model, body_type, colour, seats, location, cost_per_hour)
 VALUES 	('available', 'Volkswagen', 'Passat', 'sedan', 'grey', 5,  '-37.766720,145.137650', 33),
-		('available', 'Subaru', 'WRX', 'sedan', 'blue', 5,  '-37.781078,145.100199', 3),
+		('available', 'Subaru', 'Impreza WRX STI', 'sedan', 'blue', 5,  '-37.781078,145.100199', 3),
         ('unavailable', 'Ford', 'Fiesta', 'hatchback', 'silver', 5,  '-37.766720,145.137650', 15),
         ('unavailable', 'Volkswagen', 'Tiguan', 'SUV', 'white', 7,  '-37.709227,145.109037', 45),
-        ('available', 'Ferrari', '488', 'coupe', 'red', 2, '34.625479,-97.212649', 148);
+        ('available', 'Ferrari', '488', 'coupe', 'red', 2, '34.625479,-97.212649', 148),
+        ('available', 'Nissan', 'Skyline GTR', 'sedan', 'black', 5, '34.625479,-97.212649', 100),
+        ('available', 'Honda', 'Civic Type R', 'hatchback', 'red', 5, '34.625479,-97.212649', 35.25),
+        ('available', 'Mazda', 'RX7 FD', 'coupe', 'yellow', 2, '34.625479,-97.212649', 55.55);
